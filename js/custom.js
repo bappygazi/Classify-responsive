d7fec60b163e506fec0b7a85f9c2ab2d822093c3
Bappy Gazi <41175073+bappygazi@users.noreply.github.com>
2022-06-29 07:25:11 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(function () {
+    // navbar icon rotate  //
+    
+
+    // banner slider //
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        speed: 1500
+
+    });
+
+
+})
